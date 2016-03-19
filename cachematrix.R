fc97a3e26fe38f2bd618cbd50c26d61fdc6f7a5d
carolcortez <carolcortez1@hotmail.com>
2016-03-19 17:34:33 -0300

    ProgrammingAssignment2
    The two functions bellow create a special object that stores a matrix
    and cache's its inversents

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions bellow create a special object that stores a matrix and 
+## cache's its inversents
 
-## Write a short comment describing this function
+## This first function, creates a matrix object that can cache it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setinverse<-function(solve)m<<-solve
+  getinverse<-function()m
+  list(set=set,get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculares the inverse of the matrix created with the
+## above function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  x$setinverse(m)
+  m
 }
